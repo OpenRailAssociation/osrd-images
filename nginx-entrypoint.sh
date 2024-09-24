26bdd3f920c9677e392fcbfbc0895834872ce771
Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>
2024-09-24 11:34:06 +0200

    ci: fix reuse error (#4)

--- nginx-entrypoint.sh
+++ nginx-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Copyright © 2022 The OSRD Contributors
+#
+# SPDX-License-Identifier: LGPL-3.0-only
+
 : "${NGINX_PORT:=80}"
 
 sed -i "s|%NGINX_PORT%|${NGINX_PORT}|g" /etc/nginx/conf.d/nginx.conf
