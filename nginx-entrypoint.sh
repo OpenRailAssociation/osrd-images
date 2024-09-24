e568a35554de6d79805e7d9d478450c64ce6c7f2
Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>
2024-09-24 14:25:52 +0200

    reuse: add .license files to bash and conf files (#5)

--- nginx-entrypoint.sh
+++ nginx-entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Copyright © 2022 The OSRD Contributors
-#
-# SPDX-License-Identifier: LGPL-3.0-only
-
 : "${NGINX_PORT:=80}"
 
 sed -i "s|%NGINX_PORT%|${NGINX_PORT}|g" /etc/nginx/conf.d/nginx.conf
